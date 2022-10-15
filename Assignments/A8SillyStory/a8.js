ac4a9dedca9d27f0e15d2a2adaad28c29815dbf6
Kerstin Nyquist <keny7243@colorado.edu>
2022-10-14 22:57:05 -0600

    finsihes

--- Assignments/A8SillyStory/a8.js
+++ Assignments/A8SillyStory/a8.js
@@ -11,11 +11,11 @@ function randomValueFromArray(array){
 
 /*2. RAW TEXT STRINGS*/
 
-const storyText = 'It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const storyText = 'The desert winds blew wildly, bringing sand into the crevices of :insertx:\'s face. The sun had scorched the earth to 115 fahrenheit. They felt a glimmer of hope when the sand took shape from a distance. \":inserty:.\" As they approached, the sand began to :insertz:. :insertx: watched as Bob emerged from the chaos, a figure weighing 2300 pounds.';
 
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
-const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
+const insertY = ['The soup kitchen', 'Disneyland', 'The White House'];
+const insertZ = ['spontaneously combust', 'melt into a puddle', 'turn into crawling slugs'];
 
 /*3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION*/
 
@@ -39,9 +39,9 @@ function result() {
   }
 
   if(document.getElementById("uk").checked) {
-    const weight = Math.round(300/14) + ' stone';
-    const temperature = Math.round((94-32)*(5/9)) + ' centigrade';
-    newStory = newStory.replaceAll('94 fahrenheit', temperature);
+    const weight = Math.round(2300/14) + ' stone';
+    const temperature = Math.round((115-32)*(5/9)) + ' centigrade';
+    newStory = newStory.replaceAll('115 fahrenheit', temperature);
     newStory = newStory.replaceAll('300 pounds', weight);
   }
 
