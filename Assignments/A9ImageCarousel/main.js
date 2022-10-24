430791878d3a28804da606fa85bfbf5b1e12ce1d
Kerstin Nyquist <keny7243@colorado.edu>
2022-10-23 18:22:39 -0600

    finished a9

--- Assignments/A9ImageCarousel/main.js
+++ Assignments/A9ImageCarousel/main.js
@@ -11,34 +11,36 @@ const images = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg', 'pic5.jpg'];
 /* Declaring the alternative text for each image file */
 
 const altText = {
-'pic1.jpg' : 'Closeup of a human eye', 
-'pic2.jpg' : 'description2', 
-'pic3.jpg' : 'description3', 
-'pic4.jpg' : 'description4', 
-'pic5.jpg' : 'description5'}
+'pic1.jpg' : 'Closeup of a grass', 
+'pic2.jpg' : 'Back of a painted building', 
+'pic3.jpg' : 'Cut-down tree', 
+'pic4.jpg' : 'Closeup of a rock', 
+'pic5.jpg' : 'River'};
 
 /* Looping through images */
 
 for (const image of images) {
 const newImage = document.createElement('img');
-newImage.setAttribute('src', `images/${images}`);
-newImage.setAttribute('alt', altText[images]);
+newImage.setAttribute('src', `images/${image}`);
+newImage.setAttribute('alt', altText[image]);
 thumbBar.appendChild(newImage);
 
 newImage.addEventListener('click', e => {
 displayedImage.src = e.target.src;
 displayedImage.alt = e.target.altText;});
-}
+};
 
 
 /* Wiring up the Darken/Lighten button */
-/*btn.addEventListener('click', getAttribute);
-
-If the class name is "dark", changes the <button> class to "light" (using setAttribute()), its text content to "Lighten", and the background-color of the overlay <div> to "rgba(0,0,0,0.5)".
-If the class name is not "dark", changes the <button> class to "dark", its text content back to "Darken", and the background-color of the overlay <div> to "rgba(0,0,0,0)".
-The following lines provide a basis for achieving the changes stipulated in points 2 and 3 above.
-
-btn.setAttribute("class", xxx);
-btn.textContent = xxx;
-overlay.style.backgroundColor = xxx;
-getAttribute()*/
+btn.addEventListener('click', () => {
+  const btnClass = btn.getAttribute('class');
+  if (btnClass === 'dark'){
+    btn.setAttribute("class", 'light');
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = 'rgba(142,53,41,0.5)';
+  } else {
+    btn.setAttribute("class", 'dark');
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = 'rgb(0,0,0,0)';
+  }
+});
