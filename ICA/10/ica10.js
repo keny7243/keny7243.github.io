2c20aaa47740d8dc6979fc9416ef20b81b87aa99
Kerstin Nyquist <keny7243@colorado.edu>
2022-10-25 10:40:14 -0600

    finished

--- ICA/10/ica10.js
+++ ICA/10/ica10.js
@@ -37,4 +37,81 @@ class Ball {
         ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI); //generates our ball
         ctx.fill();
       }
+
+      update() {//makes the ball move, accounts for edges of screen
+        if ((this.x + this.size) >= width) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.x - this.size) <= 0) {
+          this.velX = -(this.velX);
+        }
+      
+        if ((this.y + this.size) >= height) {
+          this.velY = -(this.velY);
+        }
+      
+        if ((this.y - this.size) <= 0) {
+          this.velY = -(this.velY);
+        }
+      
+        this.x += this.velX;
+        this.y += this.velY;
+      }
+
+      collisionDetect() {
+        for (const ball of balls) {
+          if (this !== ball) { //only runs if it is not seeing if it collided with itself 
+            const dx = this.x - ball.x;
+            const dy = this.y - ball.y;
+            const distance = Math.sqrt(dx * dx + dy * dy);
+      
+            if (distance < this.size + ball.size) {// if they overlap, change both their colors to a random color
+              ball.color = this.color = randomRGB();
+            }
+          }
+        }
+      }
   }
+
+  //make a new variable, call it balls, its going to be an array
+const balls = [];
+
+//as long as we have less than 25 balls in the array, we're gonna generate a random size and make a new ball
+while (balls.length < 25) {
+  const size = random(10, 20);
+  const ball = new Ball(
+    // ball position always drawn at least one ball width
+    // away from the edge of the canvas, to avoid drawing errors
+    random(0 + size, width - size),
+    random(0 + size, height - size),
+    //x and y velocity for the two random methods below, generating a random speed between -7 to 7 pixels
+    random(-7, 7),
+    random(-7, 7),
+    randomRGB(),
+    size
+  );
+
+  balls.push(ball);
+}
+
+// function runs forever 
+function loop() {
+    //draw rectangle that fills canvas context and make it semi-transparent black
+    ctx.fillStyle = "rgba(0, 0, 0, 0.25)";
+    ctx.fillRect(0, 0, width, height);
+  
+    //for each ball in that array, we are gonna draw it and update it
+    for (const ball of balls) {
+      ball.draw();
+      ball.update();
+      ball.collisionDetect();
+    }
+  
+    requestAnimationFrame(loop);
+  }
+
+  //calls first iteration of this
+  loop();
+
+  
