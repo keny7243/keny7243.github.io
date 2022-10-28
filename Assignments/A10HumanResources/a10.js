d02f38909c5eb5378102d05a038bf63024b15cbb
Kerstin Nyquist <keny7243@colorado.edu>
2022-10-28 16:23:40 -0600

    Update a10.js

--- Assignments/A10HumanResources/a10.js
+++ Assignments/A10HumanResources/a10.js
@@ -21,9 +21,9 @@ let jsonEmployees= [
             raiseEligible : false
         }]
 
-let jsonString = JSON.stringify(jsonEmployees);
 let employeeString = "Problem 1: The employee JSON = "
-console.log(employeeString + jsonString);
+console.log(employeeString);
+console.log(jsonEmployees);
 
 
 
@@ -34,11 +34,13 @@ let jsonText = {
 }
 
 
-jsonString = JSON.stringify(jsonText);
-let companyString = "Problem 2: The company JSON = "
-console.log(companyString + jsonString);
 
-jsonEmployees.push ({
+let companyString = "Problem 2: The company JSON = "
+console.log(companyString);
+console.log(jsonText);
+
+// do this in the future
+jsonText.employees.push ({
     firstName: "Anna", 
     department : "Tech",
     designation : "Executive",
@@ -47,8 +49,8 @@ jsonEmployees.push ({
 });
 
 let addString = "Problem 3: Anna added to the JSON = "
-jsonString = JSON.stringify(jsonText);
-console.log(addString + jsonString);
+console.log(addString);
+console.log(jsonText1);
 
 let totalSalary = 0;
 for(let z = 0; z < jsonEmployees.length; z++) {
@@ -56,8 +58,9 @@ for(let z = 0; z < jsonEmployees.length; z++) {
     totalSalary = totalSalary + jsonEmployees[z].salary;
   };
 
-let salaryString = "Problem 4: The total salary of the employees is $"
-console.log(salaryString + totalSalary +".");
+let salaryString = "Problem 4: The total salary of the employees is..."
+console.log(salaryString);
+console.log(totalSalary);
 
 
 for(let z = 0; z < jsonEmployees.length; z++) {
@@ -68,8 +71,8 @@ if (jsonEmployees[z].raiseEligible === true) {
 }
 
 let raiseString = "Problem 5: Sam and Mary got a 10% raise and now are not raise eligible = "
-jsonString = JSON.stringify(jsonText);
-console.log(raiseString + jsonString);
+console.log(raiseString);
+console.log(jsonText);
 
 
 jsonEmployees.forEach(element =>
@@ -82,10 +85,5 @@ for(let z = 0; z < jsonEmployees.length; z++) {
     }
 
 let wfhString = "Problem 6: Anna and Sam like to work from home = "
-jsonString = JSON.stringify(jsonText);
-console.log(wfhString + jsonString);
-
-/*
-    It's raise time. If an employee is raise eligible, increase their salary by 10%. 
-    Given the JSON of the company and its employees, write a function to 
-    update the salary for each employee who is raised eligible, then set their eligibility to fal */
+console.log(wfhString);
+console.log(jsonText);
