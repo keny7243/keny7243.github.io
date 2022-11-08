e840dccb505c786b02de712b8d4d598b7d476bcc
Kerstin Nyquist <keny7243@colorado.edu>
2022-11-08 10:38:30 -0700

    inclass phone number work

--- WA/12/wa12.js
+++ WA/12/wa12.js
@@ -1,4 +1,62 @@
-const button = document.querySelector('.button');
+
+function check() {
+    console.log('test');
+}
+
+function submit() {
+    alert(output.textContent);
+}
+
+function reset() {
+    outputInt = 0;
+    output.textContent = outputInt;
+}
+
+function minus() {
+    if (outputInt > 0) {
+    outputInt -=1;
+    output.textContent = outputInt; }
+}
+
+function plus() {
+    if (outputInt < 9999999999) {
+    outputInt +=1;
+    output.textContent = outputInt;}
+}
+
+function random() {
+    outputInt = randomNumber(0, 9999999999);
+    output.textContent = outputInt;
+}
+function randomNumber(min, max) {
+    const num = Math.floor(Math.random() * (max-min +1) + min);
+    return num;
+}
+
+const output = document.querySelector('.output');
+let outputInt = parseInt(output.textContent);
+console.log(outputInt);
+
+const minusButton = document.querySelector('.minus-button').addEventListener('click', minus);
+const plusButton = document.querySelector('.plus-button').addEventListener('click', plus);
+const resetButton = document.querySelector('.reset-button').addEventListener('click', reset);
+const randomButton = document.querySelector('.random-button').addEventListener('click', random);
+const submitButton = document.querySelector('.submit-button').addEventListener('click', submit);
+
+
+let slider = document.getElementById('myRange');
+const sliderSubmit = document.querySelector(".slider-submit-button").addEventListener('click', update);
+let sliderOutput = document.querySelector('.slider-output');
+
+sliderOutput.oninput = function () {
+    sliderOutput.innerHTML = this.value;
+}
+
+function update() {
+    sliderOutput.textContent = slider.value;
+}
+
+/*const button = document.querySelector('.button');
 const output = document.querySelector('.output');
 let phone_content = document.querySelector('.phone');
 
@@ -6,4 +64,5 @@ button.addEventListener('click', updateOutput);
 
 function updateOutput() {
     output.textContent = phone_content.value;
-}
+    alert(phone_content.value); //what the submit button should do for this assignment
+}*/
