e919a39eb9d779f01e2c6999d62accb5728da8ff
Kerstin Nyquist <keny7243@colorado.edu>
2022-12-10 15:28:54 -0700

    just about done

--- map/map.js
+++ map/map.js
@@ -1,11 +1,23 @@
-
-
 console.log(d3); //d3 object and its available functions
-const svg = d3.select('svg');//('div#container');
- /*.append('svg') //selects all svg on page and assigns svg constant to them
-  .attr("preserveAspectRatio", "xMinYMin meet")
-  .attr("viewBox", "0 0 1000 1000")
-  .classed("svg-content", true);*/
+let svg = d3.select('svg')//('div#container');
+    .append('svg') //selects all svg on page and assigns svg constant to them
+    .classed("svg-content", true);
+
+function myFunction(w) {
+    if (w.matches) { // If media query matches
+        svg
+          .attr("preserveAspectRatio", "xMinYMin meet")
+          .attr("viewBox", "0 0 850 850");
+    } else {
+        svg
+          .attr("preserveAspectRatio", "xMinYMin meet")
+          .attr("viewBox", "0 0 660 660");
+    }
+  }
+ 
+var w = window.matchMedia("(max-width: 749px)")
+  myFunction(w) // Call listener function at run time
+  w.addListener(myFunction) // Attach listener function on state changes
 
 // geoNaturalEarth1 - map projection
 // geoPath - converts the data path to svg path String
@@ -14,7 +26,7 @@ const pathGenerator = d3.geoPath().projection(projection); //instance of geopath
 
 const g = svg.append('g');
 const colorLegendG = svg.append('g')
-    .attr('transform', `translate(30,200)`); //where the colorlegend is 
+    .attr('transform', `translate(20,320)`); //where the colorlegend is 
 
 g.append('path') //appending to g
     .attr('class', 'sphere') //giving the path a class named sphere
@@ -34,6 +46,7 @@ d3.tsv('../countryData/110-m.tsv')
             countryRows[d.iso_n3] = d; //get the id's whole row of information
         }));
 
+        //https://www.npmjs.com/package/d3-scale-chromatic - different color patterns
         const colorScale = d3.scaleOrdinal(d3.schemeYlOrBr[7]);//creating color scale
         const colorValue = d => countryRows[d.id].economy;
 
@@ -55,9 +68,11 @@ const colorLegend = (selection, props) => {
     .merge(backgroundRect)
         .attr('x', -circleRadius * 2)//changing position of x so that it moves back the dimaeter of the circles
         .attr('y', -circleRadius * 2)
-        .attr('width', 300) //setting width and height
-        .attr('height', spacing * 7 + circleRadius * 2)//height is the spacing of the text times the seven options + circle radius doubled
-        .attr('fill', 'white');
+        .attr('rx', circleRadius*2) //rounding the rectangle's edges
+        .attr('width', 260) //setting width and height
+        .attr('height', spacing * colorScale.domain().length + circleRadius * 2)//height is the spacing of the text times the seven options + circle radius doubled
+        .attr('fill', 'white')//color
+        .attr('opacity', 0.8);//opacity
 
     const groups = selection.selectAll('g')
       .data(colorScale.domain());
@@ -68,7 +83,7 @@ const colorLegend = (selection, props) => {
       .merge(groups)
         .attr('transform', (d, i) =>
           `translate(0, ${i * spacing})`
-        );
+        ).on('click', d => console.log(d));//doesn't show names, just that pointer event occurred - can't proceed with interactive video
     groups.exit().remove();
   
     groupsEnter.append('circle') //making the circles and adding color
@@ -86,8 +101,7 @@ const colorLegend = (selection, props) => {
 
 //make a json using the same d numbers, adding image files and biodiversity index
 //then add to legend or text at the bottom
-https://unpkg.com/world-atlas@1.1.4/world/110m.json
-d3.json('110m.json') //load data of world polygons using d3.json -> returns promise
+d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json') //load data of world polygons using d3.json -> returns promise
     .then (data => { //then (access data and create function that uses data as input)
         console.log(data);
         const countries = topojson.feature(data, data.objects.countries); //converts topojson into geojson?
@@ -96,6 +110,7 @@ d3.json('110m.json') //load data of world polygons using d3.json -> returns prom
         colorScale
             .domain(countries.features.map(colorValue))
             .domain(colorScale.domain().sort().reverse());
+            /*.range(schemeSpectral[colorScale.domain().length]);*/
         console.log(colorScale.domain());
 
         colorLegendG.call(colorLegend, {
@@ -113,24 +128,15 @@ d3.json('110m.json') //load data of world polygons using d3.json -> returns prom
                 //d is a string containing a series of path commands that define the path to be drawn ----> https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d
                 .attr('fill', d=> colorScale(colorValue(d)))//fill is based on income data
                 .append('title') //can't just add attribute because this is child of svg
-                .text(d => countryRows[d.id].name + ": " + colorValue(d) + ", "+ countryRows[d.id].income_grp + ", " + countryRows[d.id].continent)
-    });
-
-/*    const image = document.querySelector('img'); //adds reference to the img element
-
-    const quoteButton = document.querySelector('.button'); //adds reference to the class button
-    quoteButton.addEventListener('click', getUser); //if clicked, run the getUser function
-
-    async function getUser() { //defining getUser function    
-        console.log(jsonResponse);
-    
-        displayImage(jsonResponse['results'][0]['picture']['large']);
-        displayName(jsonResponse['results'][0]['name']['first'] + ' ' + jsonResponse['results'][0]['name']['last']);
-        imageAlt(jsonResponse['results'][0]['name']['first'] + ' ' + jsonResponse['results'][0]['name']['last']);
-        displayUsername(jsonResponse['results'][0]['login']['username']);
-        displayAge(jsonResponse['results'][0]['dob']['age'].toString());
-    }
-
+                .on('click', d => { //tried to create image - no errors but nothing is happening
+                    if (countryRows[d.id].continent = Africa){ //not working, but also not showing any errors
+                        image.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Flag_of_the_People%27s_Republic_of_China.svg/1280px-Flag_of_the_People%27s_Republic_of_China.svg.png';
+                    }
+                     //going down the continent list
+                    })
+                .text(d => countryRows[d.id].name + ": " + colorValue(d).substring(3) + ", with "+ countryRows[d.id].income_grp.substring(3).toLowerCase() + " in " + countryRows[d.id].continent + ".")
+                
+            });
 
    /* Create <img> element in the HTML code.
 Add style to <img> element and set display properties to none.
