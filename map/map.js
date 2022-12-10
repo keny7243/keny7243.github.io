4b34d19c8eafe826e4fe71fc0cbc8ed62c82dd3e
Kerstin Nyquist <keny7243@colorado.edu>
2022-12-09 22:10:19 -0700

    adding a lot

--- map/map.js
+++ map/map.js
@@ -1,10 +1,15 @@
 console.log(d3); //d3 object and its available functions
-const svg = d3.select('svg'); //selects all svg on page and assigns svg constant to them
+const svg = d3.select('svg');//('div#container');
+ /*.append('svg') //selects all svg on page and assigns svg constant to them
+  .attr("preserveAspectRatio", "xMinYMin meet")
+  .attr("viewBox", "0 0 1000 1000")
+  .classed("svg-content", true);*/
 
 // geoNaturalEarth1 - map projection
 // geoPath - converts the data path to svg path String
 const projection = d3.geoNaturalEarth1(); //instance of the map projection
 const pathGenerator = d3.geoPath().projection(projection); //instance of geopath with projection as method with projection variable
+
 
 const g = svg.append('g');
 
@@ -12,15 +17,21 @@ g.append('path') //appending to g
     .attr('class', 'sphere') //giving the path a class named sphere
     .attr('d', pathGenerator({type: 'Sphere'})); //form of the globe
 
-svg.call(d3.zoom().on('zoom', () => { //zoom when zoomed
-    g.attr('transform', event.transform);
-}));
+svg.call(d3.zoom().on('zoom', (e) => { //zoom when zoomed - had to put e so that transform would work
+    g.attr('transform', e.transform);
+    }));
 
-const countryName = {};
-d3.tsv('https://unpkg.com/world-atlas@1.1.4/world/110m.tsv')
+/*function handleZoom(e) {
+    d3.select('g.chart')
+      .attr('transform', e.transform);
+  }*/
+
+//https://unpkg.com/world-atlas@1.1.4/world/110m.tsv
+const countryRows = {};
+d3.tsv('../countryData/110m.tsv')
     .then(data => 
         data.forEach(d => { //for each id from data, 
-            countryName[d.iso_n3] = d.name; //get the id's name
+            countryRows[d.iso_n3] = d; //get the id's whole row of information
         }));
 
 //use d.name for country name
@@ -39,7 +50,7 @@ d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json') //load data of wo
                 .attr('d', pathGenerator)//set the d attribute of these paths based on the country
                 //d is a string containing a series of path commands that define the path to be drawn ----> https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d
             .append('title') //can't just add attribute because this is child of svg
-                .text(d => countryName[d.id]); //gets country name from tsv
+                .text(d => countryRows[d.id].name); //gets country name from tsv
     });
 
 //go to d3 to see how to style css of .text
