cd74e08ce63c282af11dc265143c52a6b5bc71ea
Kerstin Nyquist <keny7243@colorado.edu>
2022-12-10 13:25:16 -0700

    more added

--- map/map.js
+++ map/map.js
@@ -1,3 +1,5 @@
+
+
 console.log(d3); //d3 object and its available functions
 const svg = d3.select('svg');//('div#container');
  /*.append('svg') //selects all svg on page and assigns svg constant to them
@@ -12,7 +14,7 @@ const pathGenerator = d3.geoPath().projection(projection); //instance of geopath
 
 const g = svg.append('g');
 const colorLegendG = svg.append('g')
-    .attr('transform', `translate(180,150)`);
+    .attr('transform', `translate(30,200)`); //where the colorlegend is 
 
 g.append('path') //appending to g
     .attr('class', 'sphere') //giving the path a class named sphere
@@ -23,12 +25,6 @@ svg.call(d3.zoom().on('zoom', (e) => { //zoom when zoomed - had to put e so that
     }));
 
  
-  /* colorLegendG.call(colorLegend, {
-      colorScale,
-      circleRadius: 30,
-      spacing: 80,
-      textOffset: 40
-    });*/
 
 //https://unpkg.com/world-atlas@1.1.4/world/110m.tsv
 const countryRows = {};
@@ -44,10 +40,54 @@ d3.tsv('../countryData/110-m.tsv')
 //use d.name for country name
 //use d.iso_n3 for id
 
+//COLORLEGEND
+const colorLegend = (selection, props) => {
+    const {
+      colorScale,
+      circleRadius,
+      spacing,
+      textOffset
+    } = props;
+  
+    const backgroundRect = selection.selectAll('rect')//selects all rectangnles
+    .data([null]);
+    backgroundRect.enter().append('rect')
+    .merge(backgroundRect)
+        .attr('x', -circleRadius * 2)//changing position of x so that it moves back the dimaeter of the circles
+        .attr('y', -circleRadius * 2)
+        .attr('width', 300) //setting width and height
+        .attr('height', spacing * 7 + circleRadius * 2)//height is the spacing of the text times the seven options + circle radius doubled
+        .attr('fill', 'white');
+
+    const groups = selection.selectAll('g')
+      .data(colorScale.domain());
+    const groupsEnter = groups
+      .enter().append('g')
+        .attr('class', 'tick');
+    groupsEnter
+      .merge(groups)
+        .attr('transform', (d, i) =>
+          `translate(0, ${i * spacing})`
+        );
+    groups.exit().remove();
+  
+    groupsEnter.append('circle') //making the circles and adding color
+      .merge(groups.select('circle'))
+        .attr('r', circleRadius)
+        .attr('fill', colorScale);
+  
+    groupsEnter.append('text') //making the text
+      .merge(groups.select('text'))
+        .text(d => d)
+        .attr('dy', '0.32em')
+        .attr('x', textOffset);
+  }
+//END OF COLOR LEGEND
+
 //make a json using the same d numbers, adding image files and biodiversity index
 //then add to legend or text at the bottom
-
-d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json') //load data of world polygons using d3.json -> returns promise
+https://unpkg.com/world-atlas@1.1.4/world/110m.json
+d3.json('110m.json') //load data of world polygons using d3.json -> returns promise
     .then (data => { //then (access data and create function that uses data as input)
         console.log(data);
         const countries = topojson.feature(data, data.objects.countries); //converts topojson into geojson?
@@ -55,8 +95,15 @@ d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json') //load data of wo
         
         colorScale
             .domain(countries.features.map(colorValue))
-            .domain(colorScale.domain().sort());
+            .domain(colorScale.domain().sort().reverse());
         console.log(colorScale.domain());
+
+        colorLegendG.call(colorLegend, {
+      colorScale,
+      circleRadius: 7,
+      spacing: 20,
+      textOffset: 9
+    });
 
         g.selectAll('path')//selectng all paths in g
             .data(countries.features)//data join for our paths of features
@@ -66,7 +113,27 @@ d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json') //load data of wo
                 //d is a string containing a series of path commands that define the path to be drawn ----> https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d
                 .attr('fill', d=> colorScale(colorValue(d)))//fill is based on income data
                 .append('title') //can't just add attribute because this is child of svg
-                .text(d => countryRows[d.id].name); //gets country name from tsv
+                .text(d => countryRows[d.id].name + ": " + colorValue(d) + ", "+ countryRows[d.id].income_grp + ", " + countryRows[d.id].continent)
     });
 
-//go to d3 to see how to style css of .text
+/*    const image = document.querySelector('img'); //adds reference to the img element
+
+    const quoteButton = document.querySelector('.button'); //adds reference to the class button
+    quoteButton.addEventListener('click', getUser); //if clicked, run the getUser function
+
+    async function getUser() { //defining getUser function    
+        console.log(jsonResponse);
+    
+        displayImage(jsonResponse['results'][0]['picture']['large']);
+        displayName(jsonResponse['results'][0]['name']['first'] + ' ' + jsonResponse['results'][0]['name']['last']);
+        imageAlt(jsonResponse['results'][0]['name']['first'] + ' ' + jsonResponse['results'][0]['name']['last']);
+        displayUsername(jsonResponse['results'][0]['login']['username']);
+        displayAge(jsonResponse['results'][0]['dob']['age'].toString());
+    }
+
+
+   /* Create <img> element in the HTML code.
+Add style to <img> element and set display properties to none.
+Create a JavaScript “show()” function that can access the image and change the display property to block.
+Add button in HTML code which calls “show()” function when user clicks on it.
+//go to d3 to see how to style css of .text*/
